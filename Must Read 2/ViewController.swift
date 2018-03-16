6f74756daa6a5650e3c689cbf5422da7fbc5feda
Kseniia <hellokseniia@gmail.com>
2018-03-16 21:42:09 +0200

    Initial Commit

---
+++ Must Read 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Must Read 2
+//
+//  Created by Xenia Rastvorova on 16.03.2018.
+//  Copyright © 2018 Kseniia Rastvorova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
