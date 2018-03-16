6016495318d000f4ff0911547e2156a19bb0e120
Stanislav Rastvorov <sircfc@me.com>
2018-03-16 22:56:42 +0200

    Testing CocoaPods sync.

--- Must Read 2/ViewController.swift
+++ Must Read 2/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Test!\nAnd it really works!")
+        print("Test!\nAnd it really works!\nAnd now we have CocoaPods!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
