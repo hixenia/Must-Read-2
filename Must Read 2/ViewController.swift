ec0e4fac8d680bebee7ddd80b0671d044dfc115d
Stanislav Rastvorov <sircfc@me.com>
2018-03-16 22:01:05 +0200

    One more print change.

--- Must Read 2/ViewController.swift
+++ Must Read 2/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Test")
+        print("Test!\nAnd it really works!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
